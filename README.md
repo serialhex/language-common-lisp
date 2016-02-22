d7862da6ba35e50916548a19a67a35899e15724b
Justin Patera <serialhex@gmail.com>
2016-02-21 22:31:28 -0500

    initial commit, got most numbers working & keywords

---
+++ README.md
@@ -0,0 +1,7 @@
+# Common Lisp Highlighting & Support in Atom
+
+An awesome package lovingly created to support proper syntax highlighting of Common Lisp files.
+
+This is *not* converted from the Textmate Bundle, and therefore does not have the shortcomings of said bundle!
+
+![A screenshot of your package](https://f.cloud.github.com/assets/69169/2290250/c35d867a-a017-11e3-86be-cd7c5bf3ff9b.gif)
---
+++ test.lisp
@@ -0,0 +1,61 @@
+
+; single-line comment
+
+#|
+  multiline
+  comment
+  (defun meh ()
+    'llama)
+|#
+
+t
+nil
+true
+nilhism
+
+:keyword
+:nil-word
+
+'(710 ; int
+  -1  ; int
+  9.  ; yes, int: clhs 2.3.1 Numbers as Tokens
+  1/3 ; ratio
+  -2/3  ; ratio
+  -3.5  ; float
+  1.0D0 ; float
+  .9    ; float
+  -.8   ; float
+  1f0   ; float
+  4.S-5 ; float
+  -.6l7 ; float
+  1.0e-15) ; float
+#B101100 ; binary
+#b091100  ; bad binary
+#o12357   ; octal
+#O8675309() ; bad octal
+#XaBcD56  ; hex
+#xz632g   ; bad hex
+#6rmoO92  ; this would be way complicated...
+#35RfD45  ; all the digits
+#37r2hfy  ; bad random radix
+#c(6f-3 -9/8)     ; complex numbers yo!
+#C(0.7 8.6 3/6) ; bad complex number
+
+'symbol
+#'function-symbol
+
+(defun foo->quix (foo)
+  "i change foos into quixs"
+  (declare (ignore foo))
+  'quix)
+
+(defmacro bar (baz quix)
+  "string for documentationz"
+  macro!)
+
+(defsomething thing (meh)
+  "I define somethings")
+
+'(i am a quoted list)
+
+#P"C:\\am\\a/long filepath"
